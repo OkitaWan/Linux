2cd618b802caa6cd0f1330612984d3dd3ee2a237
Okita <962874559@qq.com>
2019-10-14 19:11:43 -0700

    test static

---
+++ code/test/tests.c
@@ -0,0 +1,9 @@
+#include "/home/rlk/linuxprogram/week6/code/staticlib/lib/lib.h"
+int main()
+{	
+	int a[UNUMBER];
+	urand(a, UNUMBER);
+	printf("max=%d  \n", max(a, UNUMBER));
+	printf("sum=%d  \n", sum(a, UNUMBER));
+	return 0;
+}
