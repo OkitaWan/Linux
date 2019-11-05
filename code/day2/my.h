b6d6cb295c9b5e055d1790f0bfd3807181615a57
Okita <962874559@qq.com>
2019-11-04 22:09:23 -0800

    week9 day2 classwork

---
+++ code/day2/my.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+#include <wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
